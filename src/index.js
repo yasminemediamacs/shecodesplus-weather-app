67be5da723cb5653ce4eb17e6a4ebe9352cbea99
yasminemediamacs <yasmine@mediamacs.com>
2021-06-17 16:16:52 +0200

    Added the search engine

--- src/index.js
+++ src/index.js
@@ -43,8 +43,19 @@ function displayTemperature(response) {
   iconElement.setAttribute("alt", response.data.weather[0].description);
 }
 
-let apiKey = "4972edbb64b9d2ca6d473a6ca13f3e9b";
-let city = "Bolzano";
-let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+function search(city) {
+  let apiKey = "4972edbb64b9d2ca6d473a6ca13f3e9b";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(displayTemperature);
+}
 
-axios.get(apiUrl).then(displayTemperature);
+function handleSubmit(event) {
+  event.preventDefault();
+  let cityInputElement = document.querySelector("#city-input");
+  search(cityInputElement.value);
+}
+
+let form = document.querySelector("#search-form");
+form.addEventListener("submit", handleSubmit);
+
+search("Bolzano");
